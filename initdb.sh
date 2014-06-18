3333cd2236d6cf19c419fba43ff83fd9c317da53
Jorden Verwer <jorden.verwer@goabout.com>
2014-06-18 10:52:33 +0200

    Accept any user name from environment in initdb.sh

--- initdb.sh
+++ initdb.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 echo Dropping db...
 sudo -u postgres dropdb otpprofiler
 echo Creating db...
-sudo -u postgres createdb -O otpprofiler otpprofiler
+sudo -u postgres createdb -O $USER otpprofiler
 echo Applying schema...
 psql -d otpprofiler -f ./schema.sql
 # endpoints should be generated once and stored with baseline graph inputs
@@ -12,4 +12,3 @@ psql -d otpprofiler -f ./schema.sql
 #     -o endpoints_random.csv endpoints -n 50 -s --radius 2000 
 echo Populating request parameters and endpoints tables...
 python ./populate_db.py
-
