5bb029fb47e19e9d8abbe2590f0463f3b8ae0c7f
Teemu Peltonen <teemu.peltonen@gmail.com>
2017-10-20 14:55:30 +0300

    Throw exceptions and minor improvements

--- otpprofiler.py
+++ otpprofiler.py
@@ -1,27 +1,36 @@
 #!/usr/bin/python
+from __future__ import print_function
 
-import urllib2, time, itertools, json
+from future.standard_library import install_aliases
+
+install_aliases()
+
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
+import time, itertools, json
 import subprocess, urllib, random
-import simplejson
 import pprint
 from copy import copy
 from datetime import date
 from random import randint, seed
+
+import sys
 
 # python-requests no longer has first-class support for concurrent asynchronous HTTP requests
 # the author has moved it to https://github.com/kennethreitz/grequests
 # python-requests wraps urllib2 providing a much nicer API.
 import grequests
 
-TIME='14:00:00'
+TIME = '14:00:00'
 
 # generate test date on a recent/upcoming monday. Use a fixed work day to keep results comparable
 cdate = date.today()
-dd = cdate.day - cdate.weekday() # monday = 0. want monday!
-if dd < 1:
-    dd = dd + 7 # use next monday
+dd = cdate.day - cdate.weekday()  # monday = 0. want monday!
+dd += 7  # use next monday
 
-DATE = "%s-%s-%s"%(cdate.year, cdate.month, dd)
+DATE = "%s-%s-%s" % (cdate.year, cdate.month, dd)
 
 # split out base and specific endpoint
 SHOW_PARAMS = False
@@ -32,9 +41,10 @@ SHOW_RESPONSE = False
 response_json = []
 full_itins_json = []
 pp = pprint.PrettyPrinter(indent=4)
-n = 0 # number of responses received
-N = 0 # total number of responses expected
-t0 = 0 # time that search begins
+n = 0  # number of responses received
+N = 0  # total number of responses expected
+t0 = 0  # time that search begins
+
 
 def cycle(seq):
     "A generator that loops over a sequence forever."
@@ -45,6 +55,7 @@ def cycle(seq):
         for elem in seq:
             yield elem
 
+
 def pairs(iterable):
     "A generator that takes items from a sequence two at a time. (s0, s1), (s2, s3), (s4, s5), ..."
     it = iter(iterable)
@@ -52,28 +63,29 @@ def pairs(iterable):
         yield (x, next(it, None))
 
 
-def get_params(fast, count):
-    requests_json = simplejson.load(open("requests.json"))
+def get_params(fast, count, filename="requests.json", requests_json=None):
+    if requests_json is None and filename is not None:
+        requests_json = json.load(open(filename))
     requests = requests_json['requests']
     endpoints = requests_json['endpoints']
-    elen = len(endpoints);
+    elen = len(endpoints)
 
     if elen < 2:
-        print "Not enough endpoints"
+        print("Not enough endpoints")
         exit()
-    print "test count=%d"%count
+    print("test count=%d" % count)
 
     if elen > 2 * count:
-        endpoints = endpoints[:2*count]
+        endpoints = endpoints[:2 * count]
     elif elen < 2 * count:
         newpoints = []
         pairIds = {}
         i = 0
         prevr = 0
-        seed(1) # use a fixed random sequence to get repeatable results
-        for j in range (0, count*10): # apply a large top limit to ensure the loop ends
-            r = randint(0, elen-1)
-            if i%2 != 0:
+        seed(1)  # use a fixed random sequence to get repeatable results
+        for j in range(0, count * 10):  # apply a large top limit to ensure the loop ends
+            r = randint(0, elen - 1)
+            if i % 2 != 0:
                 pair_id = str(prevr) + "_" + str(r)
                 if pair_id in pairIds:
                     # point pair already exists, pick a new one
@@ -82,14 +94,14 @@ def get_params(fast, count):
             # got a valid new endpoint, continue with new endpoint
             newpoints.append(endpoints[r])
             prevr = r
-            i+=1
-            if i >= count*2:
-                break #done
+            i += 1
+            if i >= count * 2:
+                break  # done
 
         endpoints = newpoints
 
     # Make sure there is an even number of endpoints
-    if len(endpoints) % 2 != 0 :
+    if len(endpoints) % 2 != 0:
         endpoints = endpoints[:-1]
 
     ret = []
@@ -99,11 +111,13 @@ def get_params(fast, count):
         req = copy(request)
         req['oid'] = origin['id']
         req['tid'] = target['id']
-        req['fromPlace'] = "%s,%s"%(origin['lat'],origin['lon'])
-        req['toPlace']   = "%s,%s"%(target['lat'],target['lon'])
-        ret.append( req )
+        req['fromPlace'] = "%s,%s" % (origin['lat'], origin['lon'])
+        req['toPlace'] = "%s,%s" % (target['lat'], target['lon'])
+        ret.append(req)
+
     # TODO yield
     return ret
+
 
 def getServerInfo(host):
     """Get information about the server that is being profiled. Returns a tuple of:
@@ -113,37 +127,38 @@ def getServerInfo(host):
     cpuName is the model of the cpu as reported by /proc/cpuinfo via the OTP serverinfo API,
     and nCores is the number of (logical) cores reported by that same API, including hyperthreading.
     """
-    url_meta = "http://"+host+"/otp/"
+    url_meta = "http://" + host + "/otp/"
 
-    try :
-        print "grabbing metadata from %s"%url_meta
-        req = urllib2.Request(url_meta)
+    try:
+        print("grabbing metadata from %s" % url_meta)
+        req = Request(url_meta)
         req.add_header('Accept', 'application/json')
-        response = urllib2.urlopen(req)
-        if response.code != 200 :
-            print "Server metadata response was not 200"
+        response = urlopen(req)
+        if response.code != 200:
+            print("Server metadata response was not 200")
             return None
         content = response.read()
         objs = json.loads(content)
-        print "Server metadata response: ", objs
+        print("Server metadata response: ", objs)
         version = objs['serverVersion']['version']
         sha1 = objs['serverVersion']['commit']
         # would also be nice to have build date
         cpuName = objs['cpuName']
         nCores = objs['nCores']
-    except urllib2.URLError :
+    except:
         # This is a normal condition while waiting for server to come up, trap exception and return None.
         # Any problem with decoding the server response will still blow up the program with an exception.
-        print "Error requesting metadata from OTP server. Is it running?"
+        print("Error requesting metadata from OTP server. Is it running?")
         return None
-    print "sha1 of commit is:", sha1
-    print "version of OTP is:", version
-    print "processor type is:", cpuName
-    print "number of logical cores is:", nCores
+    print("sha1 of commit is:", sha1)
+    print("version of OTP is:", version)
+    print("processor type is:", cpuName)
+    print("number of logical cores is:", nCores)
     return (sha1, version, cpuName, nCores)
 
+
 # summarize the result for an itinerary planner request
-def summarize_plan (itinerary) :
+def summarize_plan(itinerary):
     routes = []
     trips = []
     waits = []
@@ -151,35 +166,37 @@ def summarize_plan (itinerary) :
     leg_times = []
     n_vehicles = 0
     n_legs = 0
-    for leg in itinerary['legs'] :
+    for leg in itinerary['legs']:
         n_legs += 1
         leg_modes.append(leg['mode'])
-        leg_times.append((leg['endTime'] - leg['startTime'])/1000)
-        if 'route' in leg and len(leg['route']) > 0 :
+        leg_times.append((leg['endTime'] - leg['startTime']) / 1000)
+        if 'route' in leg and len(leg['route']) > 0:
             routes.append(leg['route'])
             trips.append(leg['tripId'])
             n_vehicles += 1
-            wait = (leg['from']['departure'] - leg['from']['arrival'])/1000
-            #print(' - wait = %d'%(wait))
+            wait = (leg['from']['departure'] - leg['from']['arrival'] if 'arrival' in leg['from'] else leg['from'][
+                'departure']) / 1000
+            # print(' - wait = %d'%(wait))
             waits.append(wait)
     ret = {
-        'start_time' : time.asctime(time.gmtime(itinerary['startTime'] / 1000)) + ' GMT',
-        'duration' : '%d sec' % int(itinerary['duration']),
-        'n_legs' : n_legs,
-        'n_vehicles' : n_vehicles,
-        'walk_distance' : itinerary['walkDistance'],
-        'wait_time_sec' : itinerary['waitingTime'],
-        'ride_time_sec' : itinerary['transitTime'],
-        'routes' : routes,
-        'trips' : trips,
-        'waits' : waits,
-        'leg_modes' : leg_modes,
-        'leg_times' : leg_times
+        'start_time': time.asctime(time.gmtime(itinerary['startTime'] / 1000)) + ' GMT',
+        'duration': '%d sec' % int(itinerary['duration']),
+        'n_legs': n_legs,
+        'n_vehicles': n_vehicles,
+        'walk_distance': itinerary['walkDistance'],
+        'wait_time_sec': itinerary['waitingTime'],
+        'ride_time_sec': itinerary['transitTime'],
+        'routes': routes,
+        'trips': trips,
+        'waits': waits,
+        'leg_modes': leg_modes,
+        'leg_times': leg_times
     }
     return ret
 
+
 # summarize the result for a profile request
-def summarize_profile (option) :
+def summarize_profile(option):
     routes = []
     trips = []
     waits = []
@@ -225,19 +242,19 @@ def summarize_profile (option) :
         summarize_profile_non_transit_leg(option['access'], leg_modes, leg_times)
 
     ret = {
-        'n_legs' : n_legs,
-        'n_vehicles' : n_vehicles,
-        'wait_time_sec' : wait_time_sec,
-        'ride_time_sec' : ride_time_sec,
-        'routes' : routes,
-        'waits' : waits,
-        'leg_modes' : leg_modes,
-        'leg_times' : leg_times
+        'n_legs': n_legs,
+        'n_vehicles': n_vehicles,
+        'wait_time_sec': wait_time_sec,
+        'ride_time_sec': ride_time_sec,
+        'routes': routes,
+        'waits': waits,
+        'leg_modes': leg_modes,
+        'leg_times': leg_times
     }
     return ret
 
 
-def summarize_profile_non_transit_leg (leg, leg_modes, leg_times) :
+def summarize_profile_non_transit_leg(leg, leg_modes, leg_times):
     if len(leg) == 1:
         leg_modes.append(leg[0]['mode'])
         leg_times.append(leg[0]['time'])
@@ -250,20 +267,22 @@ def summarize_profile_non_transit_leg (leg, leg_modes, leg_times) :
         leg_modes.append(modes)
         leg_times.append(times)
 
+
 # Generate a callback closure containing the unfinished row.
 # We could potentially avoid this by only saving the URL or query parameters, and not passing in a row.
-def response_callback_factory(row, profile) :
-    def handle_response(response, *args, **kwargs) :
-        global n # avoid "referenced before assignment" weirdness
+def response_callback_factory(row, profile):
+    def handle_response(response, *args, **kwargs):
+        global n  # avoid "referenced before assignment" weirdness
         n += 1
         t = (time.time() - t0) / 60.0
         T = (N * t) / n
-        print "Request %d/%d, time %0.2f min of %0.2f (estimated) received" % (n, N, t, T), response,
+        print("Request %d/%d, time %0.2f min of %0.2f (estimated) received" % (n, N, t, T), response, )
         n_itin = 0
         elapsed = 0
-        if response.status_code != 200 :
+        itineraries = []
+        if response.status_code != 200:
             status = 'failed'
-        else :
+        else:
             row['itins'] = []
             objs = response.json()
             if profile:
@@ -278,8 +297,13 @@ def response_callback_factory(row, profile) :
                     status = 'no paths'
             else:
                 row['query_type'] = 'plan'
-                row['debug'] = objs['debugOutput']
-                elapsed = objs['debugOutput']['totalTime']
+                if 'debugOutput' in objs:
+                    row['debug'] = objs['debugOutput']
+                    elapsed = objs['debugOutput']['totalTime']
+                else:
+                    row['debug'] = None
+                    elapsed = 0
+
                 if 'plan' in objs:
                     itineraries = objs['plan']['itineraries']
                     n_itin = len(itineraries)
@@ -291,52 +315,60 @@ def response_callback_factory(row, profile) :
                 row['total_time'] = str(elapsed) + ' msec'
                 row['avg_time'] = None if n_itin == 0 else '%f msec' % (float(elapsed) / n_itin)
 
-        print status
-        response_id = len(response_json) # not threadsafe -- not atomic with following line
-        response_json.append( row )
+        print(status)
+        response_id = len(response_json)  # not threadsafe -- not atomic with following line
+        response_json.append(row)
         row['status'] = response.status_code
         row['response_id'] = response_id
 
         # Create a row for each itinerary/option within this single trip planner result
         if profile:
-            for (option_number, option) in enumerate(options) :
-                option_row = summarize_profile (option)
+            for (option_number, option) in enumerate(options):
+                option_row = summarize_profile(option)
                 option_row['itinerary_number'] = option_number + 1
-                full_itin = {'response_id':response_id,'itinerary_number':option_number+1}
-                full_itin['body']=option
-                full_itins_json.append( full_itin )
-                row['itins'].append( option_row )
+                full_itin = {'response_id': response_id, 'itinerary_number': option_number + 1}
+                full_itin['body'] = option
+                full_itins_json.append(full_itin)
+                row['itins'].append(option_row)
         else:
-            for (itinerary_number, itinerary) in enumerate(itineraries) :
-                itin_row = summarize_plan (itinerary)
-                #itin_row['response_id'] = response_id
+            for (itinerary_number, itinerary) in enumerate(itineraries):
+                itin_row = summarize_plan(itinerary)
+                # itin_row['response_id'] = response_id
                 itin_row['itinerary_number'] = itinerary_number + 1
-                full_itin = {'response_id':response_id,'itinerary_number':itinerary_number+1}
-                full_itin['body']=itinerary
-                full_itins_json.append( full_itin )
-                row['itins'].append( itin_row )
+                full_itin = {'response_id': response_id, 'itinerary_number': itinerary_number + 1}
+                full_itin['body'] = itinerary
+                full_itins_json.append(full_itin)
+                row['itins'].append(itin_row)
 
-        if SHOW_RESPONSE :
+        if SHOW_RESPONSE:
             pp.pprint(row)
         response.connection.close();
+
     # return the function definition, a closure for a specific instance of 'row'
     return handle_response
 
 
-def run(connect_args) :
+def run(connect_args, requests_json=None):
+    global t0, N, response_json, full_itins_json, n  # HACK
+    response_json = []
+    full_itins_json = []
+    n = 0  # number of responses received
+    N = 0  # total number of responses expected
+    t0 = 0  # time that search begins
+
     "This is the principal function..."
     notes = connect_args.pop('notes')
     retry = connect_args.pop('retry')
-    fast  = connect_args.pop('fast')
+    fast = connect_args.pop('fast')
     count = connect_args.pop('count')
     host = connect_args.pop('host')
     profile = connect_args.pop('profile')
     Date = connect_args.pop('date')
     Time = connect_args.pop('time')
 
-    print "TEST DATE:", Date, Time
+    print("TEST DATE:", Date, Time)
 
-    print("profile=%s"%profile)
+    print("profile=%s" % profile)
     # info = getServerInfo(host)
     # while retry > 0 and info == None:
     #     print "Failed to connect to OTP server. Waiting to retry (%d)." % retry
@@ -351,15 +383,15 @@ def run(connect_args) :
     # Create a dict describing this particular run of the profiler, which will be output as JSON
     run_time_id = int(time.time())
     run_row = (notes, run_time_id)
-    run_json = dict(zip(('notes','id'), run_row))
+    run_json = dict(zip(('notes', 'id'), run_row))
 
-    all_params = get_params(fast, count)
-    global t0, N # HACK
+    all_params = get_params(fast, count, requests_json=requests_json)
+
     t0 = time.time()
     N = len(all_params)
     reqs = []
-    for params in all_params :
-        params = dict(params) # TODO necessary?
+    for params in all_params:
+        params = dict(params)  # TODO necessary?
         request_id = params.pop('id')
         oid = params.pop('oid')
         tid = params.pop('tid')
@@ -376,7 +408,7 @@ def run(connect_args) :
             api_method = 'plan'
             params['numItineraries'] = 1
 
-        qstring = urllib.urlencode(params)
+        qstring = urlencode(params)
 
         if "http" in host:
             url = host
@@ -390,20 +422,22 @@ def run(connect_args) :
         if not url.endswith('/'):
             url = url + "/"
 
-        url = "%s%s?%s"%(url, api_method, qstring)
+        url = "%s%s?%s" % (url, api_method, qstring)
 
         # Tomcat server + spaces in URLs -> HTTP 505 confusion
-        if SHOW_PARAMS :
+        if SHOW_PARAMS:
             pp.pprint(params)
-        if SHOW_URL :
-            print url
-        row = { 'url' : url,
-                'run_id' : run_time_id,
-                'request_id' : request_id,
-                'origin_id' : oid,
-                'target_id' : tid,
-                'id_tuple' : "%s-%s-%s"%(oid,tid,request_id),
-                'membytes' : None }
+
+        if SHOW_URL:
+            print(url)
+        row = {'url': url,
+               'run_id': run_time_id,
+               'request_id': request_id,
+               'origin_id': oid,
+               'target_id': tid,
+               'id_tuple': "%s-%s-%s" % (oid, tid, request_id),
+               'mode': params['mode'] if 'mode' in params else params['modes'],
+               'membytes': None, 'from': params['fromPlace'], 'to': params['toPlace']}
         # You can't give arguments to the response callback, you have to make a factory function:
         # "http://stackoverflow.com/questions/25115151/how-to-pass-parameters-to-hooks-in-python-grequests"
         # Closures are created in Python by function calls.
@@ -412,22 +446,34 @@ def run(connect_args) :
         req = grequests.get(url, headers=headers, hooks=dict(response=response_callback))
         reqs.append(req)
 
+    def exception_handler(request, exception):
+        raise exception
+
     # Set max number of concurrent requests to 20, OTP should throttle this via worker threads
-    grequests.map(reqs, size=20)
+    grequests.map(reqs, size=5, exception_handler=exception_handler)
 
     # Write out all results at the end. Really, this should probably be done in streaming fashion.
-    fpout = open("run_summary.%s.json"%run_time_id,"w")
+
     run_json['responses'] = response_json
-    simplejson.dump(run_json, fpout, indent=2)
+
+    '''
+    fpout = open("run_summary.%s.json" % run_time_id, "w")
+    
+    json.dump(run_json, fpout, indent=2)
     fpout.close()
 
-    fpout = open("full_itins.%s.json"%run_time_id,"w")
-    simplejson.dump( full_itins_json, fpout, indent=2 )
+    fpout = open("full_itins.%s.json" % run_time_id, "w")
+    json.dump(full_itins_json, fpout, indent=2)
     fpout.close()
+    '''
+    return run_json
+
 
 import argparse
-if __name__=="__main__":
-    import argparse # optparse is deprecated
+
+if __name__ == "__main__":
+    import argparse  # optparse is deprecated
+
     parser = argparse.ArgumentParser(description='perform an otp profiler run')
     parser.add_argument('host')
     parser.add_argument('-f', '--fast', action='store_true', default=False)
